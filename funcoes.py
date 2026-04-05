8d025b9a0f39d51d4346eab9b77d77d9c1605388
Raquel Bomjardim <114450946+Bomjardimraquel@users.noreply.github.com>
2026-04-05 12:09:18 -0300

    corrigindo soma

--- funcoes.py
+++ funcoes.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def soma(df, categorias, tipo="Saída"):
-    return -df[(df["Categoria"].isin(categorias)) & (df["Tipo"] == tipo)]["Valor"].sum()
+    return df[(df["Categoria"].isin(categorias)) & (df["Tipo"] == tipo)]["Valor"].sum()
 
 def soma_entradas(df, categorias):
     return df[(df["Categoria"].isin(categorias)) & (df["Tipo"] == "Entrada")]["Valor"].sum()
